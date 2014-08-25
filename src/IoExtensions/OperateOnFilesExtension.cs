7ff33c0b99fc4b1679ee95e15240ea06179544c9
Simon Baynes <baynezy@gmail.com>
2014-08-25 15:39:56 +0100

    Added NuGet package restore Added OperateOnFilesExtension

---
+++ src/IoExtensions/OperateOnFilesExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using LinqExtensions;
+
+namespace IoExtensions
+{
+	public static class OperateOnFilesExtension
+	{
+		public static void OperateOnFiles(this DirectoryInfo directory, string search, Action<FileInfo> action)
+		{
+			directory.EnumerateFiles(search).Each(action.Invoke);
+		}
+	}
+}
